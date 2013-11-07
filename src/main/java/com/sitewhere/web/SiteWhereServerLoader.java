943ea7760e71c043031580bc57dcf9595bba23dd
Derek Adams <derek.adams@reveal-tech.com>
2013-11-07 18:12:19 -0500

    Better error output in server loader. Added HBase module to server configuration.

--- src/main/java/com/sitewhere/web/SiteWhereServerLoader.java
+++ src/main/java/com/sitewhere/web/SiteWhereServerLoader.java
@@ -1,19 +1,23 @@
 /*
-* $Id$
-* --------------------------------------------------------------------------------------
-* Copyright (c) Reveal Technologies, LLC. All rights reserved. http://www.reveal-tech.com
-*
-* The software in this package is published under the terms of the CPAL v1.0
-* license, a copy of which has been included with this distribution in the
-* LICENSE.txt file.
-*/
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) Reveal Technologies, LLC. All rights reserved. http://www.reveal-tech.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 
 package com.sitewhere.web;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 import org.apache.log4j.Logger;
+import org.mule.util.StringMessageUtils;
 
 import com.sitewhere.server.SiteWhereServer;
 import com.sitewhere.spi.SiteWhereException;
@@ -43,9 +47,23 @@ public class SiteWhereServerLoader extends HttpServlet {
 			SiteWhereServer.getInstance().create();
 			SiteWhereServer.getInstance().start();
 		} catch (SiteWhereException e) {
+			List<String> messages = new ArrayList<String>();
+			messages.add("!!!! SiteWhere Server Failed to Start !!!!");
+			messages.add("");
+			messages.add("Error: " + e.getMessage());
+			String message = StringMessageUtils.getBoilerPlate(messages, '*', 60);
+			LOGGER.info("\n" + message + "\n");
 			LOGGER.error(e);
+			e.printStackTrace();
 		} catch (Throwable e) {
+			List<String> messages = new ArrayList<String>();
+			messages.add("!!!! Unhandled Exception !!!!");
+			messages.add("");
+			messages.add("Error: " + e.getMessage());
+			String message = StringMessageUtils.getBoilerPlate(messages, '*', 60);
+			LOGGER.info("\n" + message + "\n");
 			LOGGER.error(e);
+			e.printStackTrace();
 		}
 	}
 }
